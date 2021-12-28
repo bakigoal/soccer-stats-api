e74bf7857a46695e6dff608c965b183eab3ef77d
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-28 22:53:45 +0500

    initial commit

---
+++ src/main/kotlin/com/bakigoal/soccerstatsapi/SoccerStatsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.bakigoal.soccerstatsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoccerStatsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoccerStatsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/bakigoal/soccerstatsapi/SoccerStatsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bakigoal.soccerstatsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoccerStatsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
